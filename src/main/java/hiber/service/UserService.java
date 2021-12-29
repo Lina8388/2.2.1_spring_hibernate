b8d2979516fc6f39687a54d4fcf4c2a38c6fc5bf
Lina8388 <v.sem93@mail.ru>
2021-12-29 17:45:01 +0300

    Initial commit

---
+++ src/main/java/hiber/service/UserService.java
@@ -0,0 +1,11 @@
+package hiber.service;
+
+import hiber.model.User;
+import java.util.List;
+
+public interface UserService {
+    void add(User user);
+    List<User> listUsers();
+
+    List<User> getUserWithCar(String model, int series);
+}
